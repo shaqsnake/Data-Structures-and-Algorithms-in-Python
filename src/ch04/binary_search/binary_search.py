8200e7bc9ff7bbe0e372bbe30dc4be002bac4a99
tanzhuxiaqiu <tanzhuxiaqiu@huawei.com>
2018-11-22 10:40:53 +0800

    Add some comments to binary_search.py

--- src/ch04/binary_search/binary_search.py
+++ src/ch04/binary_search/binary_search.py
@@ -1,4 +1,6 @@
 def binary_search(data, target, low, high):
+    """Return position if target is found in indicated portion of a python list and -1 if target is not found.
+    """
     if low > high:
         return -1
 
@@ -6,6 +8,8 @@ def binary_search(data, target, low, high):
     if target == data[mid]:
         return mid
     elif target < data[mid]:
+        # recur on the portion left of the middle
         return binary_search(data, target, low, mid-1)
     else:
+        # recur on the portion right of the middle
         return binary_search(data, target, mid+1, high)
