c559d39360fe657a49c95325bd0aaa76efc924e6
tanzhuxiaqiu <tanzhuxiaqiu@huawei.com>
2018-11-30 16:04:17 +0800

    Update sequence_iterator add main

--- src/ch02/sequence_iterator/sequence_iterator.py
+++ src/ch02/sequence_iterator/sequence_iterator.py
@@ -17,3 +17,9 @@ class SequenceIterator:
     def __iter__(self):
         """By convention, an iterator must return itself as an iterator."""
         return self
+
+
+if __name__ == "__main__":
+    si = SequenceIterator([ x for x in range(10)])
+    for i in range(10):
+        print(next(si))
