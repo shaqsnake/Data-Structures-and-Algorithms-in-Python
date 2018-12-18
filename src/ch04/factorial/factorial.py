b71a2f4e0ae22fbed4e077d44696f3a41b78f758
tanzhuxiaqiu <tanzhuxiaqiu@huawei.com>
2018-12-18 15:05:07 +0800

    add decorator clock to factorial

--- src/ch04/factorial/factorial.py
+++ src/ch04/factorial/factorial.py
@@ -1,4 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../.."))
+
+from utils.clockdeco import clock
+
+
+@clock
 def factorial(n):
     if n == 0:
         return 1
-    return n * factorial(n-1)
+    return n * factorial(n - 1)
+
+
+if __name__ == "__main__":
+    factorial(6)
