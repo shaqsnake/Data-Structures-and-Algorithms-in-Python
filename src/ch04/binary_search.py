a17db01d8a52733a3ea870841b72b60124de8800
tanzhuxiaqiu <tanzhuxiaqiu@huawei.com>
2018-12-18 15:29:32 +0800

    restucture the source code, move utils under the ch04 folder

--- src/ch04/binary_search.py
+++ src/ch04/binary_search.py
@@ -1,3 +1,7 @@
+from utils.clockdeco import clock
+
+
+@clock
 def binary_search(data, target, low, high):
     """Return position if target is found in indicated portion of a python list and -1 if target is not found.
     """
@@ -13,3 +17,8 @@ def binary_search(data, target, low, high):
     else:
         # recur on the portion right of the middle
         return binary_search(data, target, mid + 1, high)
+
+
+if __name__ == "__main__":
+    data = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+    binary_search(data, 23, 0, len(data)-1)
--- src/ch04/factorial.py
+++ src/ch04/factorial.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../.."))
-
 from utils.clockdeco import clock
 
 
