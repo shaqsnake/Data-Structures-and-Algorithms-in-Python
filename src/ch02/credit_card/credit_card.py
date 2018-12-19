052c09451000c21d8ee3c2f49da5ccf39021fb6e
tanzhuxiaqiu <tanzhuxiaqiu@huawei.com>
2018-12-19 10:18:53 +0800

    Update format of source code

--- src/ch02/credit_card/credit_card.py
+++ src/ch02/credit_card/credit_card.py
@@ -1,6 +1,6 @@
 class CreditCard:
     """A consumer credit card."""
-    
+
     def __init__(self, customer, bank, acnt, limit):
         """Create a new credit card instance.
         
@@ -16,27 +16,27 @@ class CreditCard:
         self._account = acnt
         self._limit = limit
         self._balance = 0
-        
+
     def get_customer(self):
         """Return name of the customer."""
         return self._customer
-    
+
     def get_bank(self):
         """Return the bank's name."""
         return self._bank
-    
+
     def get_account(self):
         """Return the card identifying number(typically stored as a string)."""
         return self._account
-    
+
     def get_limit(self):
         """Return current credit limit."""
         return self._limit
-    
+
     def get_balance(self):
         """Return current balance."""
         return self._balance
-    
+
     def charge(self, price):
         """Charge given price to the card, assuming sufficient credit limit.
         
@@ -46,7 +46,7 @@ class CreditCard:
             return False
         self._balance += price
         return True
-    
+
     def make_payment(self, amount):
         """Process customer payment that reduces balance."""
         self._balance -= amount
@@ -60,11 +60,13 @@ if __name__ == '__main__':
 
     for val in range(1, 17):
         wallet[0].charge(val)
-        wallet[1].charge(val*2)
-        wallet[2].charge(val*3)
-        
+        wallet[1].charge(val * 2)
+        wallet[2].charge(val * 3)
+
     for c in range(3):
-        print("Customer: %s, Bank: %s, Limit: %d\nBalance: %d" % (wallet[c].get_customer(), wallet[c].get_bank(), wallet[c].get_limit(), wallet[c].get_balance()))
+        print("Customer: %s, Bank: %s, Limit: %d\nBalance: %d" %
+              (wallet[c].get_customer(), wallet[c].get_bank(),
+               wallet[c].get_limit(), wallet[c].get_balance()))
         while wallet[c].get_balance() > 100:
             wallet[c].make_payment(100)
             print("New balance: ", wallet[c].get_balance())
--- src/ch04/test_binary_search.py
+++ src/ch04/test_binary_search.py
@@ -1,5 +1,6 @@
 from binary_search import binary_search
 
+
 def test_binary_search():
     d = [1, 2, 5, 6, 7, 9, 10, 13, 16, 17, 21, 24, 27, 33]
     low = 0
