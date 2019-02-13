d9d344f6d041a8bcb62f83ad3820f382355e2949
tanzhuxiaqiu <tanzhuxiaqiu@huawei.com>
2019-02-13 15:37:27 +0800

    Update ch07, add circular queue based on linked list.

--- src/ch07/singlyl_linked_list/circular_queue.py
+++ src/ch07/singlyl_linked_list/circular_queue.py
@@ -2,51 +2,54 @@ from abstract_queue import AbstractQueue
 from node import SinglyLinkedListNode
 
 
-class LinkedListQueue(AbstractQueue):
-
+class CircularQueue(AbstractQueue):
     def __init__(self):
         super().__init__()
-        self._head = None
         self._tail = None
 
     def __iter__(self):
-        p = self._head
-        while p is not None:
+        p = self._tail._next
+        while True:
             yield p._element
+            if p is self._tail:
+                return
             p = p._next
 
     def enqueue(self, e):
         node = SinglyLinkedListNode(e)
         if self.is_empty():
-            self._head = node
+            node._next = node
         else:
+            node._next = self._tail._next
             self._tail._next = node
         self._tail = node
         self._size += 1
 
     def dequeue(self):
-        if self.is_empty():
-            raise IndexError("Queue is empty")
-        res = self._head._element
-        self._head = self._head._next
-        self._size -= 1
-        if self.is_empty():
+        remove_node = self._tail._next
+        if len(self) == 1:
             self._tail = None
-        return res
+        else:
+            self._tail._next = remove_node._next
+        self._size -= 1
+        return remove_node._element
 
     def peek(self):
-        if self.is_empty():
-            raise IndexError("Queue is empty")
-        return self._head._element
+        return self._tail._next._element
+
+    def rotate(self):
+        if len(self) > 0:
+            self._tail = self._tail._next
 
 
 if __name__ == "__main__":
-    queue = LinkedListQueue()
+    queue = CircularQueue()
     for i in range(5):
         queue.enqueue(i)
     print(queue)
 
     print(queue.dequeue())
     print(queue.dequeue())
-    print(queue.dequeue())
+    print(queue)
+    queue.rotate()
     print(queue)
